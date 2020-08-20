503832449f728e3e48987e42f5776ee490e604c8
chris <43684610+chrisfroeschl@users.noreply.github.com>
2020-08-20 14:33:03 +0200

    Update dev-tmux.sh

--- dev-tmux.sh
+++ dev-tmux.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 tmux new-session -n main \; \
-    send-keys 'cd Development/projects' C-m \; \
-    split-window -h -p 65 \; \
-    split-window -h \; \
-    select-pane -t 2 \; \
-    send-keys 'cd Development/projects' C-m \; \
-    split-window -v \; \
-    send-keys 'cd Development/projects' C-m \; \
-    select-pane -t 1 \; \
-    split-window -v -p 30 \; \
+    send-keys 'brew services' C-m \; \
+    split-window -h -p 50 \; \
+    send-keys 'cd Development' C-m \; \
+    split-window -v -p 30\; \
     send-keys 'htop' C-m \; \
-    new-window -n secondary \; \
-    split-window -h -p 65 \; \
-    send-keys 'torsocks irssi' C-m \; \
+    select-pane -t 1 \; \
     split-window -h \; \
+    send-keys 'cd Development' C-m \; \
+    new-window -n secondary \; \
     send-keys 'newsboat' C-m \; \
+    split-window -h -p 50 \; \
+    send-keys 'torsocks irssi' C-m \; \
     split-window -v \; \
     send-keys 't' C-m \; \
