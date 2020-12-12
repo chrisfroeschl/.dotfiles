732c190dfd9e4a0d90459c8ce733578574002408
Chris Fröschl <cfroeschl@protonmail.com>
2020-12-12 21:17:28 +0100

    Add meta data to groff macro files and create missing types

---
+++ groff/.config/groff/macros.mm
@@ -0,0 +1,9 @@
+\" File: macros.mm
+\" Maintainer: Chris Fröschl <cfroeschl@protonmail.com>
+\" Last Modified: Sat 12 Dec 2020 09:16:11 PM CET
+\" License:
+\" Copyright (c) Chris Fröschl. Distributed under the same terms as Vim itself.
+\" See :help license
+\"
+\" Description:
+\" General .mm macros.
