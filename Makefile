4d189fa4ad5955756c2e43e09030c1c3ec837af4
Chris Fröschl <cfroeschl@protonmail.com>
2020-12-13 09:41:18 +0100

    Update troff meta snippet to prefix with a point to avoid spacing, Add some groff macro skeletons

--- Makefile
+++ Makefile
@@ -1,3 +1,10 @@
+# File: Makefile
+# Maintainer: Chris Fröschl <cfroeschl@protonmail.com>
+# Last Modified: Sun 13 Dec 2020 09:39:54 AM CET
+# License:
+# Copyright (c) Chris Fröschl. Distributed under the same terms as Vim itself.
+# See :help license
+
 .PHONY: clean all
 
 all:
--- groff/.config/groff/macros.mm
+++ groff/.config/groff/macros.mm
@@ -1,9 +1,9 @@
-\" File: macros.mm
-\" Maintainer: Chris Fröschl <cfroeschl@protonmail.com>
-\" Last Modified: Sat 12 Dec 2020 09:16:11 PM CET
-\" License:
-\" Copyright (c) Chris Fröschl. Distributed under the same terms as Vim itself.
-\" See :help license
-\"
-\" Description:
-\" General .mm macros.
+.\" File: macros.mm
+.\" Maintainer: Chris Fröschl <cfroeschl@protonmail.com>
+.\" Last Modified: Sun 13 Dec 2020 09:38:09 AM CET
+.\" License:
+.\" Copyright (c) Chris Fröschl. Distributed under the same terms as Vim itself.
+.\" See :help license
+.\"
+.\" Description:
+.\" General .mm macros.
