b6888f92b9f47bbc344a113096f9b676bf22d136
Chris Fröschl <cfroeschl@protonmail.com>
2020-12-19 17:20:44 +0100

    Avoid some minimal bashism, Set tmux shell, Add shebang snippet to Vim

--- sfeed/.sfeed/archive.sh
+++ sfeed/.sfeed/archive.sh
@@ -1,13 +1,13 @@
+#!/bin/sh
 # File: archive.sh
 # Maintainer: Chris Fröschl <cfroeschl@protonmail.com>
-# Last Modified: Sun 13 Dec 2020 04:07:09 PM CET
+# Last Modified: Sat 19 Dec 2020 05:01:47 PM CET
 # License:
 # Copyright (c) 2020 Chris Fröschl.
 #
 # Description:
 # Cronjob: Archive entries that are older than one week.
 
-#!/bin/sh
 for feed in feeds/*; do
     awk -F '\t' -v "old=$(($(date +'%s') - 604800))" 'int($1) > old' < "${feed}" > "${feed}.new"
     mv "${feed}" "backup/${feed##*/}.bak"
--- sfeed/.sfeed/update.sh
+++ sfeed/.sfeed/update.sh
@@ -1,12 +1,12 @@
+#!/bin/sh
 # File: update.sh
 # Maintainer: Chris Fröschl <cfroeschl@protonmail.com>
-# Last Modified: Sun 13 Dec 2020 04:07:13 PM CET
+# Last Modified: Sat 19 Dec 2020 05:01:30 PM CET
 # License:
 # Copyright (c) 2020 Chris Fröschl.
 #
 # Description:
 # Cronjob: Update feeds directory and create HMTL file.
 
-#!/bin/sh
 PATH=/usr/local/bin:/usr/local/sbin:~/bin:/usr/bin:/bin:/usr/sbin:/sbin
 sfeed_update && sfeed_html $HOME/.sfeed/feeds/* > $HOME/.sfeed/feeds.html
