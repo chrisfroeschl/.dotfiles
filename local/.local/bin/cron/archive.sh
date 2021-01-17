39a14084ac03eabebc90b61ac87c997bacba44bd
Chris Fröschl <cfroeschl@protonmail.com>
2021-01-17 16:30:41 +0100

    Fix sfeed archive cronjob to be unbound to location

--- local/.local/bin/cron/archive.sh
+++ local/.local/bin/cron/archive.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 # File: archive.sh
 # Maintainer: Chris Fröschl <cfroeschl@protonmail.com>
-# Last Modified: Sat 19 Dec 2020 05:01:47 PM CET
+# Last Modified: Sun 17 Jan 2021 04:29:33 PM CET
 # License:
 # Copyright (c) 2020 Chris Fröschl.
 #
 # Description:
-# Cronjob: Archive entries that are older than one week.
+# Cronjob: Archive feeds that are older than one week.
 
-for feed in feeds/*; do
+for feed in $HOME/.sfeed/feeds/*; do
     awk -F '\t' -v "old=$(($(date +'%s') - 604800))" 'int($1) > old' < "${feed}" > "${feed}.new"
-    mv "${feed}" "backup/${feed##*/}.bak"
+    mv "${feed}" "$HOME/.sfeed/backup/${feed##*/}.bak"
     mv "${feed}.new" "${feed}"
 done
