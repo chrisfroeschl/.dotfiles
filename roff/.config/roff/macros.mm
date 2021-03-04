637f62f92dbd4c8afdcc4298d14f3c59ebc724be
Chris Fröschl <cfroeschl@protonmail.com>
2021-03-04 16:34:18 +0100

    Move all troff macros into one file, Update mime type in mkgroff to match update

--- roff/.config/roff/macros.mm
+++
@@ -1,8 +0,0 @@
-.\" File: macros.mm
-.\" Maintainer: Chris Fröschl <cfroeschl@protonmail.com>
-.\" Last Modified: Sun 13 Dec 2020 03:21:15 PM CET
-.\" License:
-.\" Copyright (c) Chris Fröschl. Distributed under the same terms as Groff itself.
-.\"
-.\" Description:
-.\" General .mm macros.
